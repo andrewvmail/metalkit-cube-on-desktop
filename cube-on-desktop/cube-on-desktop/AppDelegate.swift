8d79f8fae5c406328508d13ca81ff81636754ca0
Andrew Tan <andrewvmail@gmail.com>
2020-10-08 01:37:57 -0700

    transparent overlay for the cube to live in

--- cube-on-desktop/cube-on-desktop/AppDelegate.swift
+++ cube-on-desktop/cube-on-desktop/AppDelegate.swift
@@ -20,9 +20,23 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
         // Create the window and set the content view.
         window = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
-            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
-            backing: .buffered, defer: false)
+            contentRect: NSRect(
+                x: 0,
+                y: 0,
+                width: Environment.swidth!,
+                height: Environment.sheight!
+            ),
+            styleMask: [ .fullSizeContentView ],
+            backing: .buffered,
+            defer: false)
+        window!.backgroundColor = NSColor.clear
+        window!.ignoresMouseEvents = true
+        window!.collectionBehavior = [.transient, .ignoresCycle]
+        window!.isOpaque = true
+        window!.level = .statusBar
+        window!.contentBorderThickness(for: NSRectEdge(rectEdge: CGRectEdge(rawValue: 1)!))
+        window!.titleVisibility = .visible
+        
         window.isReleasedWhenClosed = false
         window.center()
         window.setFrameAutosaveName("Main Window")
---
+++ cube-on-desktop/cube-on-desktop/Environment.swift
@@ -0,0 +1,14 @@
+//
+//  Environment.swift
+//  cube-on-desktop
+//
+//  Created by Andrew Tan on 2020-10-08.
+//
+
+import Foundation
+import Cocoa
+
+struct Environment {
+    static let sheight = NSScreen.main?.frame.height
+    static let swidth = NSScreen.main?.frame.width
+}
